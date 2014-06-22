5a29e2f4350a93431a9207b8072ce054b39ca177
manitaka <manitaka@gmail.com>
2014-06-22 17:30:55 +0900

    First commit.

---
+++ SwiftLesson/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftLesson
+//
+//  Created by takashi on 6/22/14.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
